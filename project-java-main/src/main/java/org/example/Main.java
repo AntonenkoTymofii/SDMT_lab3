0ee787fc05c81d4d2e89ed5033281198f7f491cc
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-04-06 21:29:12 +0300

    create 3 folders with projects

---
+++ project-java-main/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! " +
+                "This is my Java simple program for SDMT lab 3"
+        );
+    }
+}
