24c51ce9ed0a2f544b91ac21f7bfa51aba9e01b8
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-05-11 19:14:56 +0300

    correct run of dockerfile

--- java-project/src/main/java/org/example/javaproject/controllers/HelloWorldController.java
+++ java-project/src/main/java/org/example/javaproject/controllers/HelloWorldController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping
 public class HelloWorldController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String sayHello () {
         return "Hello World";
     }
